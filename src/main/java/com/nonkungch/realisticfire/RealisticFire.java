0a914affde173ff707b937bad27b2941eb5c1ace
NonKung Ch <92234807+NonKungCh1@users.noreply.github.com>
2025-10-16 17:07:22 +0700

    Create RealisticFire.java

---
+++ src/main/java/com/nonkungch/realisticfire/RealisticFire.java
@@ -0,0 +1,19 @@
+
+package com.nonkungh.realisticfire;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RealisticFire extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // ลงทะเบียน Listener ของเรา
+        getServer().getPluginManager().registerEvents(new FireExtinguishListener(), this);
+        getLogger().info("RealisticFire Plugin has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("RealisticFire Plugin has been disabled!");
+    }
+}
